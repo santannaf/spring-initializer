59a27683083abaf25575895ce66ba0e8511d2552
thales farias <thalesfarias@MacBook-Pro-de-thales.local>
2025-01-20 16:19:29 -0300

    add rm

--- templates/template.restapi.build.gradle.kts
+++ templates/template.restapi.build.gradle.kts
@@ -9,3 +9,64 @@ plugins {
 dependencies {
     
 }
+
+tasks.bootJar {
+    archiveFileName.set("app.jar")
+}
+
+tasks.register<Exec>("extractJar") {
+    group = "build"
+    description = "Extracts the JAR file contents"
+    val jarFile = file("./build/libs/app.jar")
+    commandLine("jar", "-xvf", jarFile.absolutePath)
+}
+
+tasks.register<Exec>("runCustomJar") {
+    group = "application"
+    description = "Execute custom jar"
+    dependsOn("bootJar")
+    val name = "app.jar"
+    val address = "./build/libs/$name"
+    commandLine(
+        "java",
+        "-agentlib:native-image-agent=config-merge-dir=./src/main/resources/META-INF/native-image/",
+        "-jar",
+        address
+    )
+}
+
+// "-cp .:BOOT-INF/classes:`find BOOT-INF/lib | tr '\\n' ':'`",
+// "BOOT-INF/classes:BOOT-INF/lib/*",
+tasks.register<Exec>("runCustomBootJar") {
+    group = "application"
+    description = "Execute custom boot jar"
+    dependsOn("bootJar")
+    val mainClass = "santannaf.custody.RestApplicationKt"
+    commandLine(
+        "java",
+        "-agentlib:native-image-agent=config-merge-dir=./src/main/resources/META-INF/native-image/",
+        "-cp", "BOOT-INF/classes:BOOT-INF/lib/*",
+        mainClass
+    )
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            val module = "stocks-custody-rest-api"
+            imageName.set("app")
+            configurationFileDirectories.from(file("$module/src/main/resources/META-INF/native-image/"))
+            mainClass.set("santannaf.custody.RestApplicationKt")
+            verbose.set(true)
+            debug.set(true)
+            buildArgs(
+                "-J-Dfile.encoding=UTF-8",
+                "-J-Duser.country=BR",
+                "--enable-preview",
+                "-march=native",
+                "--color=always",
+                "--allow-incomplete-classpath"
+            )
+        }
+    }
+}
